ed0da53fdf184a33fd23ddfea6dfdc162af19925
jsmolka <jsmolka@pm.me>
2020-08-21 13:31:22 +0200

    use correct mask type, remove argument type

--- include/eggcpt/locale.h
+++ include/eggcpt/locale.h
@@ -8,19 +8,18 @@ namespace eggcpt
 namespace detail
 {
 
-template<typename Char, std::size_t Type>
-class IsCharType
+template<typename Char, typename std::ctype<Char>::mask Mask>
+class IsClassifiedAs
 {
 public:
-    using argument_type = Char;
     using result_type = bool;
 
-    explicit IsCharType(const std::locale& locale = std::locale())
+    explicit IsClassifiedAs(const std::locale& locale = std::locale())
         : locale(locale) {}
 
     bool operator()(const Char& ch) const
     {
-        return std::use_facet<std::ctype<Char>>(locale).is(Type, ch);
+        return std::use_facet<std::ctype<Char>>(locale).is(Mask, ch);
     }
 
 private:
@@ -29,25 +28,24 @@ private:
 
 }  // namespace detail
 
-template<typename Char> using IsAlnum  = detail::IsCharType<Char, std::ctype_base::alnum>;
-template<typename Char> using IsAlpha  = detail::IsCharType<Char, std::ctype_base::alpha>;
-template<typename Char> using IsBlank  = detail::IsCharType<Char, std::ctype_base::blank>;
-template<typename Char> using IsCntrl  = detail::IsCharType<Char, std::ctype_base::cntrl>;
-template<typename Char> using IsDigit  = detail::IsCharType<Char, std::ctype_base::digit>;
-template<typename Char> using IsGraph  = detail::IsCharType<Char, std::ctype_base::graph>;
-template<typename Char> using IsLower  = detail::IsCharType<Char, std::ctype_base::lower>;
-template<typename Char> using IsPrint  = detail::IsCharType<Char, std::ctype_base::print>;
-template<typename Char> using IsPunct  = detail::IsCharType<Char, std::ctype_base::punct>;
-template<typename Char> using IsSpace  = detail::IsCharType<Char, std::ctype_base::space>;
-template<typename Char> using IsUpper  = detail::IsCharType<Char, std::ctype_base::upper>;
-template<typename Char> using IsLower  = detail::IsCharType<Char, std::ctype_base::lower>;
-template<typename Char> using IsXDigit = detail::IsCharType<Char, std::ctype_base::xdigit>;
+template<typename Char> using IsAlnum  = detail::IsClassifiedAs<Char, std::ctype_base::alnum>;
+template<typename Char> using IsAlpha  = detail::IsClassifiedAs<Char, std::ctype_base::alpha>;
+template<typename Char> using IsBlank  = detail::IsClassifiedAs<Char, std::ctype_base::blank>;
+template<typename Char> using IsCntrl  = detail::IsClassifiedAs<Char, std::ctype_base::cntrl>;
+template<typename Char> using IsDigit  = detail::IsClassifiedAs<Char, std::ctype_base::digit>;
+template<typename Char> using IsGraph  = detail::IsClassifiedAs<Char, std::ctype_base::graph>;
+template<typename Char> using IsLower  = detail::IsClassifiedAs<Char, std::ctype_base::lower>;
+template<typename Char> using IsPrint  = detail::IsClassifiedAs<Char, std::ctype_base::print>;
+template<typename Char> using IsPunct  = detail::IsClassifiedAs<Char, std::ctype_base::punct>;
+template<typename Char> using IsSpace  = detail::IsClassifiedAs<Char, std::ctype_base::space>;
+template<typename Char> using IsUpper  = detail::IsClassifiedAs<Char, std::ctype_base::upper>;
+template<typename Char> using IsLower  = detail::IsClassifiedAs<Char, std::ctype_base::lower>;
+template<typename Char> using IsXDigit = detail::IsClassifiedAs<Char, std::ctype_base::xdigit>;
 
 template<typename Char>
 class ToLower
 {
 public:
-    using argument_type = Char;
     using result_type = Char;
 
     explicit ToLower(const std::locale& locale = std::locale())
@@ -66,7 +64,6 @@ template<typename Char>
 class ToUpper
 {
 public:
-    using argument_type = Char;
     using result_type = Char;
 
     explicit ToUpper(const std::locale& locale = std::locale())
