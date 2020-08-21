416435338f99af94c19a3f93763fa86800b7d5cc
jsmolka <jsmolka@pm.me>
2020-08-21 13:19:17 +0200

    simplify locale

--- include/eggcpt/locale.h
+++ include/eggcpt/locale.h
@@ -2,243 +2,46 @@
 
 #include <locale>
 
-#include <eggcpt/macros.h>
-
 namespace eggcpt
 {
 
-template<typename Char>
-class IsAlnum
+namespace detail
+{
+
+template<typename Char, std::size_t Type>
+class IsCharType
 {
 public:
     using argument_type = Char;
     using result_type = bool;
 
-    explicit IsAlnum(const std::locale& locale = std::locale())
+    explicit IsCharType(const std::locale& locale = std::locale())
         : locale(locale) {}
 
     bool operator()(const Char& ch) const
     {
-        return std::isalnum(ch, locale);
+        return std::use_facet<std::ctype<Char>>(locale).is(Type, ch);
     }
 
 private:
     const std::locale& locale;
 };
 
-template<typename Char>
-class IsAlpha
-{
-public:
-    using argument_type = Char;
-    using result_type = bool;
+}  // namespace detail
 
-    explicit IsAlpha(const std::locale& locale = std::locale())
-        : locale(locale) {}
-
-    bool operator()(const Char& ch) const
-    {
-        return std::isalpha(ch, locale);
-    }
-
-private:
-    const std::locale& locale;
-};
-
-template<typename Char>
-class IsBlank
-{
-public:
-    using argument_type = Char;
-    using result_type = bool;
-
-    explicit IsBlank(const std::locale& locale = std::locale())
-        : locale(locale) {}
-
-    bool operator()(const Char& ch) const
-    {
-        #if EGGCPT_OS_DARWIN && EGGCPT_CC_CLANG
-        // Workaround for macOS clang using the wrong isblank
-        return std::use_facet<std::ctype<Char>>(locale).is(std::ctype_base::alpha, ch);        
-        #else
-        return std::isblank(ch, locale);
-        #endif
-    }
-
-private:
-    const std::locale& locale;
-};
-
-template<typename Char>
-class IsCntrl
-{
-public:
-    using argument_type = Char;
-    using result_type = bool;
-
-    explicit IsCntrl(const std::locale& locale = std::locale())
-        : locale(locale) {}
-
-    bool operator()(const Char& ch) const
-    {
-        return std::iscntrl(ch, locale);
-    }
-
-private:
-    const std::locale& locale;
-};
-
-template<typename Char>
-class IsDigit
-{
-public:
-    using argument_type = Char;
-    using result_type = bool;
-
-    explicit IsDigit(const std::locale& locale = std::locale())
-        : locale(locale) {}
-
-    bool operator()(const Char& ch) const
-    {
-        return std::isdigit(ch, locale);
-    }
-
-private:
-    const std::locale& locale;
-};
-
-template<typename Char>
-class IsGraph
-{
-public:
-    using argument_type = Char;
-    using result_type = bool;
-
-    explicit IsGraph(const std::locale& locale = std::locale())
-        : locale(locale) {}
-
-    bool operator()(const Char& ch) const
-    {
-        return std::isgraph(ch, locale);
-    }
-
-private:
-    const std::locale& locale;
-};
-
-template<typename Char>
-class IsLower
-{
-public:
-    using argument_type = Char;
-    using result_type = bool;
-
-    explicit IsLower(const std::locale& locale = std::locale())
-        : locale(locale) {}
-
-    bool operator()(const Char& ch) const
-    {
-        return std::islower(ch, locale);
-    }
-
-private:
-    const std::locale& locale;
-};
-
-template<typename Char>
-class IsPrint
-{
-public:
-    using argument_type = Char;
-    using result_type = bool;
-
-    explicit IsPrint(const std::locale& locale = std::locale())
-        : locale(locale) {}
-
-    bool operator()(const Char& ch) const
-    {
-        return std::isprint(ch, locale);
-    }
-
-private:
-    const std::locale& locale;
-};
-
-template<typename Char>
-class IsPunct
-{
-public:
-    using argument_type = Char;
-    using result_type = bool;
-
-    explicit IsPunct(const std::locale& locale = std::locale())
-        : locale(locale) {}
-
-    bool operator()(const Char& ch) const
-    {
-        return std::ispunct(ch, locale);
-    }
-
-private:
-    const std::locale& locale;
-};
-
-template<typename Char>
-class IsSpace
-{
-public:
-    using argument_type = Char;
-    using result_type = bool;
-
-    explicit IsSpace(const std::locale& locale = std::locale())
-        : locale(locale) {}
-
-    bool operator()(const Char& ch) const
-    {
-        return std::isspace(ch, locale);
-    }
-
-private:
-    const std::locale& locale;
-};
-
-template<typename Char>
-class IsUpper
-{
-public:
-    using argument_type = Char;
-    using result_type = bool;
-
-    explicit IsUpper(const std::locale& locale = std::locale())
-        : locale(locale) {}
-
-    bool operator()(const Char& ch) const
-    {
-        return std::isupper(ch, locale);
-    }
-
-private:
-    const std::locale& locale;
-};
-
-template<typename Char>
-class IsXDigit
-{
-public:
-    using argument_type = Char;
-    using result_type = bool;
-
-    explicit IsXDigit(const std::locale& locale = std::locale())
-        : locale(locale) {}
-
-    bool operator()(const Char& ch) const
-    {
-        return std::isxdigit(ch, locale);
-    }
-
-private:
-    const std::locale& locale;
-};
+template<typename Char> using IsAlnum  = detail::IsCharType<Char, std::ctype_base::alnum>;
+template<typename Char> using IsAlpha  = detail::IsCharType<Char, std::ctype_base::alpha>;
+template<typename Char> using IsBlank  = detail::IsCharType<Char, std::ctype_base::blank>;
+template<typename Char> using IsCntrl  = detail::IsCharType<Char, std::ctype_base::cntrl>;
+template<typename Char> using IsDigit  = detail::IsCharType<Char, std::ctype_base::digit>;
+template<typename Char> using IsGraph  = detail::IsCharType<Char, std::ctype_base::graph>;
+template<typename Char> using IsLower  = detail::IsCharType<Char, std::ctype_base::lower>;
+template<typename Char> using IsPrint  = detail::IsCharType<Char, std::ctype_base::print>;
+template<typename Char> using IsPunct  = detail::IsCharType<Char, std::ctype_base::punct>;
+template<typename Char> using IsSpace  = detail::IsCharType<Char, std::ctype_base::space>;
+template<typename Char> using IsUpper  = detail::IsCharType<Char, std::ctype_base::upper>;
+template<typename Char> using IsLower  = detail::IsCharType<Char, std::ctype_base::lower>;
+template<typename Char> using IsXDigit = detail::IsCharType<Char, std::ctype_base::xdigit>;
 
 template<typename Char>
 class ToLower
