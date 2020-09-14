cea4dc86be46f382eefe5140497c1ee0b5bbdb2e
jsmolka <jsmolka@pm.me>
2020-09-14 18:57:46 +0200

    use inline vars in traits

--- include/eggcpt/traits.h
+++ include/eggcpt/traits.h
@@ -7,16 +7,16 @@ namespace eggcpt
 {
 
 template<typename T, typename... Ts>
-constexpr bool is_any_of_v = std::disjunction_v<std::is_same<T, Ts>...>;
+inline constexpr bool is_any_of_v = std::disjunction_v<std::is_same<T, Ts>...>;
 
 template<typename T, typename... Ts>
 struct is_any_of : std::bool_constant<is_any_of_v<T, Ts...>> {};
 
 template<typename T, template<typename> typename Func, typename = std::void_t<>>
-constexpr bool is_detected_v = false;
+inline constexpr bool is_detected_v = false;
 
 template<typename T, template<typename> typename Func>
-constexpr bool is_detected_v<T, Func, std::void_t<Func<T>>> = true;
+inline constexpr bool is_detected_v<T, Func, std::void_t<Func<T>>> = true;
 
 template<typename T, template<typename> typename Func>
 struct is_detected : std::bool_constant<is_detected_v<T, Func>> {};
@@ -75,4 +75,4 @@ struct range_value
 template<typename Range>
 using range_value_t = typename range_value<Range>::type;
 
-}
+}  // namespace eggcpt
