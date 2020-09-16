7d936e8f54233356a86a3f7d5334ed544474270d
jsmolka <jsmolka@pm.me>
2020-09-16 21:50:13 +0200

    add simple hash

---
+++ include/eggcpt/hash.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <utility>
+
+namespace eggcpt
+{
+
+template<typename T>
+void hashCombine(std::size_t& seed, const T& value)
+{
+    seed ^= std::hash<T>()(value) + 0x9E37'79B9 + (seed << 6) + (seed >> 2);
+}
+
+template<typename Iterator>
+std::size_t hashRange(Iterator begin, Iterator end)
+{
+    std::size_t seed = 0;
+    for (; begin != end; ++begin)
+        hashCombine(seed, *begin);
+
+    return seed;
+}
+
+}  // namespace eggcpt
