66af76f83afcc657db1a363a7a06066a4fcda0c8
jsmolka <jsmolka@pm.me>
2020-10-18 21:13:38 +0200

    add const version for begin and end

--- include/shell/iterator.h
+++ include/shell/iterator.h
@@ -16,7 +16,9 @@ public:
         : iter_begin(begin), iter_end(end) {}
 
     Iterator begin() { return iter_begin; }
+    Iterator begin() const { return iter_begin; }
     Iterator end() { return iter_end; }
+    Iterator end() const { return iter_end; }
     const Iterator cbegin() const { return iter_begin; }
     const Iterator cend() const { return iter_end; }
 
