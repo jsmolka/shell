9f3a435c119085d2cac7c37741e46f0622f66a03
jsmolka <jsmolka@pm.me>
2021-02-11 21:44:22 +0100

    add head and tail to mp

--- include/shell/mp.h
+++ include/shell/mp.h
@@ -18,7 +18,33 @@ using nth_t = typename nth<Index, Ts...>::type;
 template<std::size_t Index, typename... Args>
 nth_t<Index, Args...> nth_element(Args&&... args)
 {
+    static_assert(Index < sizeof...(Args));
+
     return std::get<Index>(std::forward_as_tuple(std::forward<Args>(args)...));
+}
+
+template<typename... Ts>
+struct head : nth<0, Ts...> {};
+
+template<typename... Ts>
+using head_t = typename head<Ts...>::type;
+
+template<typename... Args>
+head_t<Args...> head_element(Args&&... args)
+{
+    return nth_element<0>(args...);
+}
+
+template<typename... Ts>
+struct tail : nth<sizeof...(Ts) - 1, Ts...> {};
+
+template<typename... Ts>
+using tail_t = typename tail<Ts...>::type;
+
+template<typename... Args>
+tail_t<Args...> tail_element(Args&&... args)
+{
+    return nth_element<sizeof...(Args) - 1>(args...);
 }
 
 }  // namespace shell::mp
--- test/src/tests_mp.inl
+++ test/src/tests_mp.inl
@@ -11,3 +11,22 @@ TEST_CASE("mp::nth_element")
     REQUIRE(mp::nth_element<1>(0, 1, 2) == 1);
     REQUIRE(mp::nth_element<2>(0, 1, 2) == 2);
 }
+
+TEST_CASE("mp::head")
+{
+    REQUIRE(std::is_same_v<mp::head_t<bool, long, char>, bool>);
+}
+
+TEST_CASE("mp::head_element")
+{
+    REQUIRE(mp::head_element(0, 1, 2) == 0);
+}
+TEST_CASE("mp::tail")
+{
+    REQUIRE(std::is_same_v<mp::tail_t<bool, long, char>, char>);
+}
+
+TEST_CASE("mp::tail_element")
+{
+    REQUIRE(mp::tail_element(0, 1, 2) == 2);
+}
