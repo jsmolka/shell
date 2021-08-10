3adf3181bca8cded0df7c22e0c372a39912c58cc
jsmolka <jsmolka@pm.me>
2021-08-10 19:13:12 +0200

    update fmt

--- include/shell/fmt.h
+++ include/shell/fmt.h
@@ -5,16 +5,17 @@
 #  define FMT_HEADER_ONLY_DEFINED
 #endif
 
+#include <shell/fmt/args.h>
 #include <shell/fmt/chrono.h>
 #include <shell/fmt/color.h>
 #include <shell/fmt/compile.h>
 #include <shell/fmt/core.h>
 #include <shell/fmt/format.h>
-#include <shell/fmt/locale.h>
 #include <shell/fmt/os.h>
 #include <shell/fmt/ostream.h>
 #include <shell/fmt/printf.h>
 #include <shell/fmt/ranges.h>
+#include <shell/fmt/xchar.h>
 
 #ifdef FMT_HEADER_ONLY_DEFINED
 #  undef FMT_HEADER_ONLY
